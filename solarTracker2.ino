ea887e7e4aa6d6a9855cb2b6e0f9113208654bb8
smart-life-tech <christlightworld@gmail.com>
2023-12-08 21:45:52 +0100

    modified:   solarTracker2.ino

--- solarTracker2.ino
+++ solarTracker2.ino
@@ -34,12 +34,12 @@ void setup()
         delay(20);
     }
 
-    for (int i = 0; i < 180; ++)
+    for (int i = 0; i < 180; i++)
     {
         servoVertical.write(i);
         delay(20);
     }
-    for (int i = 180; i > 0; --)
+    for (int i = 180; i > 0; i--)
     {
         servoVertical.write(i);
         delay(20);
